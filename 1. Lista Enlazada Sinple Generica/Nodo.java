d9a37d9107b4d899f3e5d13162db23717626631c
Mario-Chura <mchurapum@unsa.edu.pe>
2022-06-12 14:35:33 -0500

    Añadiendo metodo constructor y toString

--- 1. Lista Enlazada Sinple Generica/Nodo.java
+++ 1. Lista Enlazada Sinple Generica/Nodo.java
@@ -3,4 +3,14 @@ public class Nodo <T>{
     //Atributos
 	T dato;
 	Nodo <T> sig;
+
+    //Metodo constructor
+	public Nodo (T dato) {
+		this.dato = dato;
+	}
+
+	//Metodo toString
+	public String toString() {
+		return "Nodo{" + "dato="+dato+", sig="+sig+"}";
+	}
 }
