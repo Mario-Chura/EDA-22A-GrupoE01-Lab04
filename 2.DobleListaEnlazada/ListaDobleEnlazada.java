831c7112157c6b2f007aa183ae06a5f0837e4f61
Icielo23 <90292180+Icielo23@users.noreply.github.com>
2022-06-12 17:41:25 -0500

    Metodo insertionSort

--- 2.DobleListaEnlazada/ListaDobleEnlazada.java
+++ 2.DobleListaEnlazada/ListaDobleEnlazada.java
@@ -60,5 +60,35 @@ public class ListaDobleEnlazada <E extends Comparable<E>>{
 		}
 		return str;
 	}
+	
+	public long insertionSort(){
+		Node <E> key;
+		int i;
+		long nano_startTime = System.nanoTime();
+		for (int j=1; j< dimension; j=j+1) {
+			key = this.get(j);
+			i=j-1;
+
+			while(i>-1 && this.get(i).getData().compareTo(key.getData())>0)
+				i--;
+			if(i != j-1){
+				this.remove(j);
+				dimension++;
+				if(i == -1){
+					key.setNext(this.raiz);
+					this.raiz.setPrevious(key);
+					this.raiz = key;
+				}
+				else{
+					this.get(i+1).setPrevious(key);
+					key.setPrevious(this.get(i));
+					key.setNext(this.get(i+1));
+					this.get(i).setNext(key);
+				}
+			}
+		}
+		long nano_endTime = System.nanoTime();
+		return nano_endTime - nano_startTime;
+	}
   
   }
