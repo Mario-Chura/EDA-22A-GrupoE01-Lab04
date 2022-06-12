466b7b2da9c64597282784a55f4c93f6afd603cc
Icielo23 <90292180+Icielo23@users.noreply.github.com>
2022-06-12 17:31:27 -0500

    Constructores de node

--- 2.DobleListaEnlazada/Node.java
+++ 2.DobleListaEnlazada/Node.java
@@ -3,4 +3,29 @@ public class Node <E> {
 	private E data ;
 	private Node <E> nextNode ;
 	private Node <E> previousNode ;
+	
+	//Constructores de node
+	Node () {
+		this.data = null ;
+		this.nextNode = null ;
+		this.previousNode = null ;
+	}
+	
+	Node (E data , Node <E> nextNode , Node <E> previousNode){
+		this.data = data ;
+		this.nextNode = nextNode ;
+		this.previousNode = previousNode ;
+	}
+	
+	Node (E data , Node <E> nextNode) {
+		this.data = data ;
+		this.nextNode = nextNode ;
+		this.previousNode = null ;
+	}
+	
+	Node (E data) {
+		this.data = data ;
+		this.nextNode = null ;
+		this.previousNode = null ;
+	}
   }
