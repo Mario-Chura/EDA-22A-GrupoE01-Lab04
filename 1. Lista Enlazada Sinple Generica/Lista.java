beb7b372b90e9bd5afe12a59bf470664310360ae
Mario-Chura <mchurapum@unsa.edu.pe>
2022-06-12 14:42:15 -0500

    Añadiendo metodo para obtener datos de la lista

--- 1. Lista Enlazada Sinple Generica/Lista.java
+++ 1. Lista Enlazada Sinple Generica/Lista.java
@@ -30,4 +30,15 @@ public class Lista <T> {
 		ultimo = nodo;
 		longitud++;
 	}
+
+    public T obtener(int indice ) {
+		if (indice >= 0 && indice < longitud) {
+			Nodo <T> nodo = raiz ;
+			for (int i = 0; i < longitud && i != indice; i++) {
+				nodo = nodo.sig;
+			}
+			return nodo.dato;
+		}
+		return null;
+	}
 }
