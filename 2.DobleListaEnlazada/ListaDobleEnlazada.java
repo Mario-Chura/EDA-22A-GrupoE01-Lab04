d9c92ce1fcc5040de052617871baf2c1d43b5ade
Icielo23 <90292180+Icielo23@users.noreply.github.com>
2022-06-12 17:38:58 -0500

    Método constructor

--- 2.DobleListaEnlazada/ListaDobleEnlazada.java
+++ 2.DobleListaEnlazada/ListaDobleEnlazada.java
@@ -1,8 +1,15 @@
 public class ListaDobleEnlazada <E extends Comparable<E>>{
 
-	  //Atributos
-	  private Node <E> raiz;
-	  private Node <E> ultimo;
-	  public int dimension;
+	//Atributos
+	private Node <E> raiz;
+	private Node <E> ultimo;
+	public int dimension;
+	
+	//Metodo constructor
+	public ListaDobleEnlazada(){
+		raiz = null;
+		ultimo = null;
+		dimension = 0;
+	}
   
   }
