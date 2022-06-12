fee6697ae2ddb8ae12061eb33da469d7e917015b
Mario-Chura <mchurapum@unsa.edu.pe>
2022-06-12 14:49:32 -0500

    Importando paquetes

---
+++ 1. Lista Enlazada Sinple Generica/Ejecucion.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import com.panayotis.gnuplot.JavaPlot;
+
+public class Ejecucion  {
+    public static void main(String[] args) throws IOException {
+    
+}
