4eda6e2f4cd1c23f6d5f18dd48bb30370fbd2803
Icielo23 <90292180+Icielo23@users.noreply.github.com>
2022-06-12 17:39:40 -0500

    Métodos de la doble lista enlazada

--- 2.DobleListaEnlazada/ListaDobleEnlazada.java
+++ 2.DobleListaEnlazada/ListaDobleEnlazada.java
@@ -11,5 +11,54 @@ public class ListaDobleEnlazada <E extends Comparable<E>>{
 		ultimo = null;
 		dimension = 0;
 	}
+	
+	// Metodos
+	public void add(E x){
+		Node <E> aux = new Node(x);
+		if(isEmpty())
+			this.raiz = aux;
+		else{
+			this.ultimo.setNext(aux);
+			aux.setPrevious(this.ultimo);
+		}
+		this.ultimo = aux;
+		dimension++;
+	}
+
+	public Node<E> get(int indice) {
+		if(indice >= dimension)
+			return null;
+		Node <E> aux = raiz;
+		for(int i=0;i<indice;i++)
+			aux = aux.getNext();
+		return aux;
+	}
+	public void remove(int indice) {
+		if(indice<dimension) {
+			if(indice==0){
+				raiz=raiz.getNext();
+				raiz.setPrevious(null);
+			}
+			else {
+				Node<E> anterior=this.get(indice).getPrevious();
+				anterior.setNext(this.get(indice).getNext());
+				if(anterior.getNext() != null)
+					anterior.getNext().setPrevious(anterior);
+			}
+			dimension--;
+		}
+	}
+	
+	public boolean isEmpty(){
+		return this.raiz == null;
+	}
+	
+	public String toString(){
+		String str = "";
+		for(int i = 0; i < dimension; i++){
+			str += this.get(i).getData() + " ";
+		}
+		return str;
+	}
   
   }
