32a573821cd31341def4541044636fe7caaea051
Mario-Chura <mchurapum@unsa.edu.pe>
2022-06-12 14:54:04 -0500

    Metodo de ordenamiento

--- 1. Lista Enlazada Sinple Generica/Ejecucion.java
+++ 1. Lista Enlazada Sinple Generica/Ejecucion.java
@@ -40,5 +40,22 @@ public class Ejecucion  {
 		}
 		return lista;		
 	}
+
+    public static long insertionSort(int[] A) {
+		int key;
+		int i;
+		long nano_startTime = System.nanoTime(); 
+		for(int j=1; j<A.length; j=j+1) {
+			key = A[j];
+			i = j-1;
+			while(i>-1 && A[i]>key) {
+				A[i+1] = A[i];
+				i = i-1;
+			}
+			A[i+1] = key;
+		}
+		long nano_endTime = System.nanoTime();		
+		return nano_endTime - nano_startTime;		
+	}
     
 }
