d715565f87aa4ee4c11dc5035cf03457e032605f
Mario-Chura <mchurapum@unsa.edu.pe>
2022-06-12 14:50:19 -0500

    Definimos tamaño de nuestra lista enlazada

--- 1. Lista Enlazada Sinple Generica/Ejecucion.java
+++ 1. Lista Enlazada Sinple Generica/Ejecucion.java
@@ -6,4 +6,12 @@ import com.panayotis.gnuplot.JavaPlot;
 public class Ejecucion  {
     public static void main(String[] args) throws IOException {
     
+        //Definimos tamaño
+		int tamano;        
+		Scanner teclado = new Scanner( System.in );
+		System.out.print( "Introduzca el tamaño máximo del último arreglo: " );
+		tamano = teclado.nextInt();     
+
+    }
+    
 }
